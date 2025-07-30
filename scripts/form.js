50df80deb58de329d04d9e56afeb285bf588bfe2
David Perez <davidjared123@gmail.com>
2025-07-30 12:04:30 -0700

    adding products

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,31 @@
-// Array de productos
 const products = [
-  { id: 'prod1', name: 'Product A' },
-  { id: 'prod2', name: 'Product B' },
-  { id: 'prod3', name: 'Product C' },
-  { id: 'prod4', name: 'Product D' }
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
 
-// Llenar dinámicamente el select de productos
 const select = document.getElementById('productName');
 if (select) {
   products.forEach(product => {
